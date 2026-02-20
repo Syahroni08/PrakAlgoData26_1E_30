663239b5e4d2922f43dad3ff6c3a423dcd3988c8
Syahroni08 <syahroni08022007@gmail.com>
2026-02-20 14:36:43 +0700

    P2_Percobaan 1

---
+++ Jobsheet2/Mahasiswa30.java
@@ -0,0 +1,32 @@
+package Jobsheet2;
+
+public class Mahasiswa30 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String KelasBaru) {
+        kelas = KelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+    
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        }else if (ipk >= 3.0) {
+            return "Kinerja cukup";
+        }else  {
+            return "Kinerja kurang";        }
+    }
+}
