1f6c7e1f2d3d4c8a772a088ef03c888aec3478bc
Syahroni08 <syahroni08022007@gmail.com>
2026-02-20 15:20:40 +0700

    P2_Percobaan 3 laporan 1

--- Jobsheet2/Mahasiswa30.java
+++ Jobsheet2/Mahasiswa30.java
@@ -6,6 +6,16 @@ public class Mahasiswa30 {
     String kelas;
     double ipk;
 
+    public Mahasiswa30() {
+
+    }
+
+    public Mahasiswa30(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -18,9 +28,12 @@ public class Mahasiswa30 {
     }
 
     void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
         ipk = ipkBaru;
+    } else {
+        System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
     }
-    
+}
     String nilaiKinerja() {
         if (ipk >= 3.5) {
             return "Kinerja sangat baik";
