471995875a1fb125bfec36a3498db72ae6e0b8c5
Syahroni08 <syahroni08022007@gmail.com>
2026-02-25 14:17:46 +0700

    P2_Tugas1 modif

---
+++ Jobsheet2/Dosen30.java
@@ -0,0 +1,51 @@
+package Jobsheet2;
+
+public class Dosen30 {
+    String idDosen, nama, bidangKeahlian, st;
+    boolean statusAktif;
+    int tahunBergabung, lama;
+
+    public Dosen30() {
+    }
+
+    public Dosen30(String idDoseng, String nama, boolean statusAktif, int tahunBergabung, String bidangKeahlian ) {
+        this.idDosen = idDosen;
+        this.nama = nama;
+        this.statusAktif = statusAktif;
+        this.tahunBergabung = tahunBergabung;
+        this.bidangKeahlian = bidangKeahlian; 
+    }
+
+    void tampilInformasi() {
+        System.out.println("ID Dosen : " + idDosen);
+        System.out.println("Nama Dosen : " + nama);
+        System.out.print("Status : ");
+        if (statusAktif) {
+            System.out.println("Aktif");
+        } else {
+            System.out.println("Nonaktif");
+        }
+        System.out.println("Tahun Masuk : " + tahunBergabung);
+        System.out.println("Bidang Keahlian : " + bidangKeahlian);
+    }
+
+    void setting(boolean status) {
+        statusAktif = status;
+        if (statusAktif) {
+            System.out.println("Status menjadi Aktif");
+        } else {
+            System.out.println("Status menjadi Nonaktif");
+        }
+    }
+
+    void lamakerja(int tahunSkrng) {
+        lama = tahunSkrng - tahunBergabung;
+        System.out.println("Telah bekerja selama " + lama + " tahun");
+    }
+
+    void ubahKeahlian(String bidang) {
+        bidangKeahlian = bidang;
+        System.out.println("Bidang keahlian telah diubah menjadi : " + bidang);
+    }
+}
+    
