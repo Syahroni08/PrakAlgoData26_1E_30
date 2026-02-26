d1bff2161b9325939debae3b5620b35913c2cbdb
Syahroni08 <syahroni08022007@gmail.com>
2026-02-26 10:00:11 +0700

    P3_Percobaan 2 modifikasi

--- Praktikum03/MahasiswaDemo30.java
+++ Praktikum03/MahasiswaDemo30.java
@@ -25,6 +25,7 @@ public class MahasiswaDemo30 {
         }
         for (int i = 0; i < 3; i++) {
             System.out.println("Data Mahasiswa ke-" + (i+1));
+            arrayOfMahasiswa[i].cetakInfo();
             System.out.println("NIM : " + arrayOfMahasiswa[i].nim);
             System.out.println("Nama : " + arrayOfMahasiswa[i].nama);
             System.out.println("Kelas : " + arrayOfMahasiswa[i].kelas);
