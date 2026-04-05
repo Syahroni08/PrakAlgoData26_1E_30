cbe1dbbc81b747e28907d4cebce527aa65f10b4f
Syahroni08 <syahroni08022007@gmail.com>
2026-04-05 17:40:14 +0700

    Percobaan 2 (Main)

--- Praktikum5/SortingMain30.java
+++ Praktikum5/SortingMain30.java
@@ -11,5 +11,27 @@ public class SortingMain30 {
     dataurut1.bubbleSort();
     System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
     dataurut1.tampil();
-    }
+    System.out.println("");
+
+    int b[]= {30, 20, 2, 8, 14};
+
+    Sorting30 dataurut2 = new Sorting30(b, b.length);
+    
+    System.out.println("Data awal 2");
+    dataurut2.tampil();
+    dataurut2.SelectionSort();
+    System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+    dataurut2.tampil();
+    System.out.println("");
+
+    // int c[]= {40, 10, 4, 9, 3};
+
+    // Sorting30 dataurut3 = new Sorting30(c, c.length);
+
+    // System.out.println("Data awal 3");
+    // dataurut3.tampil();
+    // dataurut3.insertSort();
+    // System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    // dataurut3.tampil();
+    // }
 }
