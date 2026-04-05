9d2e8ee974e4b69e2ab5423f8c4bf51bc60d613f
Syahroni08 <syahroni08022007@gmail.com>
2026-04-05 17:40:56 +0700

    Percobaan 3 (Main)

--- Praktikum5/SortingMain30.java
+++ Praktikum5/SortingMain30.java
@@ -24,14 +24,14 @@ public class SortingMain30 {
     dataurut2.tampil();
     System.out.println("");
 
-    // int c[]= {40, 10, 4, 9, 3};
+    int c[]= {40, 10, 4, 9, 3};
 
-    // Sorting30 dataurut3 = new Sorting30(c, c.length);
+    Sorting30 dataurut3 = new Sorting30(c, c.length);
 
-    // System.out.println("Data awal 3");
-    // dataurut3.tampil();
-    // dataurut3.insertSort();
-    // System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
-    // dataurut3.tampil();
-    // }
+    System.out.println("Data awal 3");
+    dataurut3.tampil();
+    dataurut3.insertSort();
+    System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
+    }
 }
