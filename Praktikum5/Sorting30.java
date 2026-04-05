69b63a320b730c9f949a500f987be4499c7c362a
Syahroni08 <syahroni08022007@gmail.com>
2026-04-05 17:39:56 +0700

    Percobaan 2

--- Praktikum5/Sorting30.java
+++ Praktikum5/Sorting30.java
@@ -15,8 +15,8 @@ public class Sorting30 {
 
     void bubbleSort() {
         int temp = 0;
-        for (int i = 0; i < jumData-1; i++) {
-            for (int j = 1; j < jumData-i; j++) {
+        for (int i = 0; i < jumData - 1; i++) {
+            for (int j = 1; j < jumData - i; j++) {
                 if (data[j - 1] > data[j]) {
                     temp = data[j];
                     data[j] = data[j - 1];
@@ -32,4 +32,28 @@ public class Sorting30 {
         }
         System.out.println();
     }
+
+    void SelectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[min])
+                    min = j;
+            }
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+    }
+    // void insertSort(){
+    //     for (int i = 1; i <=data.length-1; i++) {
+    //         int temp=data[i];
+    //         int j=i-1;
+    //         while (j>=0 && data[j]>temp){
+    //             data[j+1] = data[j];
+    //             j--;
+    //         }
+    //         data[j+1]=temp;
+    //     }
+    // }
 }
