22eaf05d96c22ee36df7717e9e172138449d9d7f
Syahroni08 <syahroni08022007@gmail.com>
2026-04-06 18:28:28 +0700

    Percobaan 5

--- Praktikum5/MahasiswaBerprestasi30.java
+++ Praktikum5/MahasiswaBerprestasi30.java
@@ -15,7 +15,6 @@ public class MahasiswaBerprestasi30 {
     }
     void tampil(){
         for (Mahasiswa30 m:listMhs){
-
             m.tampilInformasi();
             System.out.println("-----------------------");
         }
@@ -23,8 +22,36 @@ public class MahasiswaBerprestasi30 {
     void bubbleSort(){
         for (int i = 0; i < listMhs.length-1; i++) {
             for (int j = 1; j < listMhs.length-i; j++) {
-                
+                if (listMhs[j].ipk>listMhs[j-1].ipk){
+                    Mahasiswa30 tmp = listMhs[j];
+                    listMhs[j]=listMhs[j-1];
+                    listMhs[j-1]=tmp;
+                }
             }
         }
     }
+    void selestionSort() {
+        for (int i = 0; i < listMhs.length-1; i++) {
+            int idxMin=i;
+            for (int j = i+1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk<listMhs[idxMin].ipk){
+                    idxMin=j;
+                }
+            }
+            Mahasiswa30 tmp = listMhs[idxMin];
+            listMhs[idxMin]=listMhs[i];
+            listMhs[i]=tmp;
+        }
+    }
+    // void insertionSort(){
+    //     for (int i = 1; i < listMhs.length; i++) {
+    //         Mahasiswa30 temp = listMhs[i];
+    //         int j=i;
+    //         while (j>0 && listMhs[j-1].ipk>temp.ipk){
+    //             listMhs[j] = listMhs[j-1];
+    //             j--;
+    //         }
+    //         listMhs[j]=temp;
+    //     }
+    // }
 }
---
+++ Praktikum5/MahasiswaDemo30.java
@@ -0,0 +1,50 @@
+package Praktikum5;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo30 {
+    public static void main(String[] args) {
+        Scanner Aroyy = new Scanner(System.in);
+        MahasiswaBerprestasi30 list = new MahasiswaBerprestasi30();
+
+        System.out.print("Masukan jumlah mahasiswa: ");
+        int no = Aroyy.nextInt();
+        Aroyy.nextLine();
+
+        for (int i = 0; i < no; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i+1));
+            
+            System.out.print("NIM   : ");
+            String nim = Aroyy.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = Aroyy.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = Aroyy.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = Aroyy.nextDouble();
+            Aroyy.nextLine();
+
+            Mahasiswa30 m = new Mahasiswa30(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("\nData sebelum sorting:");
+        list.tampil();
+
+        list.bubbleSort();
+
+        System.out.println("\nData setelah sorting (DESC):");
+        list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELETION SORT (ASC)");
+        list.selestionSort();
+        list.tampil();
+
+        // System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        // list.insertionSort();
+        // list.tampil();
+    }
+}
