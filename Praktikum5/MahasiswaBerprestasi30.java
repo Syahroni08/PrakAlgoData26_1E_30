4c4a8c57b064685df37958efd3db170ea61ff47a
Syahroni08 <syahroni08022007@gmail.com>
2026-04-09 08:00:28 +0700

    Percobaan 1

--- Praktikum5/MahasiswaBerprestasi30.java
+++ Praktikum5/MahasiswaBerprestasi30.java
@@ -47,11 +47,39 @@ public class MahasiswaBerprestasi30 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa30 temp = listMhs[i];
             int j=i;
-            while (j>0 && listMhs[j-1].ipk>temp.ipk){
+            while (j>0 && listMhs[j-1].ipk<temp.ipk){
                 listMhs[j] = listMhs[j-1];
                 j--;
             }
             listMhs[j]=temp;
         }
     }
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk==cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    void tampilPosisi(double x, int pos){
+        if (pos !=-1){
+            System.out.println("Data mahasiswa dengan IPK: " + x + " ditemukan pada indeks " + pos);
+        }else{
+            System.out.println("Data " +x+ "tidak ditemukan");
+        }
+    }
+    void tampilDataSeacrh(double x, int pos) {
+        if (pos !=-1){
+System.out.println("nim\t : " +listMhs[pos].nim);
+        System.out.println("nama\t : " +listMhs[pos].nama);
+        System.out.println("kelas\t : " +listMhs[pos].kelas);
+        System.out.println("ipk\t : " +x);
+        }else {
+        System.out.println("Data mahasiswa dengan IPK " +x+ " tidak ditemukan");
+    } 
+    }
 }
+ 
--- Praktikum5/MahasiswaDemo30.java
+++ Praktikum5/MahasiswaDemo30.java
@@ -6,12 +6,9 @@ public class MahasiswaDemo30 {
     public static void main(String[] args) {
         Scanner Aroyy = new Scanner(System.in);
         MahasiswaBerprestasi30 list = new MahasiswaBerprestasi30();
+        int jumMhs = 5; 
 
-        System.out.print("Masukan jumlah mahasiswa: ");
-        int no = Aroyy.nextInt();
-        Aroyy.nextLine();
-
-        for (int i = 0; i < no; i++) {
+        for (int i = 0; i < jumMhs; i++) {
             System.out.println("Data Mahasiswa ke-" + (i+1));
             
             System.out.print("NIM   : ");
@@ -31,20 +28,19 @@ public class MahasiswaDemo30 {
             list.tambah(m);
         }
 
-        System.out.println("\nData sebelum sorting:");
         list.tampil();
 
-        list.bubbleSort();
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = Aroyy.nextDouble();
 
-        System.out.println("\nData setelah sorting (DESC):");
-        list.tampil();
-
-        System.out.println("Data yang sudah terurut menggunakan SELETION SORT (ASC)");
-        list.selestionSort();
-        list.tampil();
-
-        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
-        list.insertionSort();
-        list.tampil();
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss = (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSeacrh(cari, pss);
     }
 }
