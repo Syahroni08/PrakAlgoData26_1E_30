159b21b0202bc181a93cbbe39813899c3ea1d61d
Syahroni08 <syahroni08022007@gmail.com>
2026-04-23 09:13:51 +0700

    Laporan 1

---
+++ Jobsheet9/StackTugasMahasiswa30.java
@@ -0,0 +1,65 @@
+package Jobsheet9;
+
+public class StackTugasMahasiswa30 {
+    Mahasiswa30[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa30(int size) {
+        this.size = size;
+        stack = new Mahasiswa30[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpaty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push (Mahasiswa30 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisas menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa30 pop() {
+        if (!isEmpaty()) {
+            Mahasiswa30 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa30 peek() {
+        if (!isEmpaty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
