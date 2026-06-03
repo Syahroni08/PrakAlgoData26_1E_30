8c8d65df60916b1a4f314d512c4379ab58ff94e6
Syahroni08 <syahroni08022007@gmail.com>
2026-06-03 12:57:17 +0700

    Percobaan 1

---
+++ Pertemuan14/Node30.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node30 {
+    Mahasiswa30 mahasiswa;
+    Node30 left, right;
+
+    public Node30() {
+
+    }
+
+    public Node30(Mahasiswa30 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
